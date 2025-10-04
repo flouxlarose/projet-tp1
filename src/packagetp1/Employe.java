07a82851fc7ce100ac3d52e5aed89532353db0b6
filou <larosefelixb@gmail.com>
2025-10-03 20:41:51 -0400

    bruh

--- src/packagetp1/Employe.java
+++ src/packagetp1/Employe.java
@@ -8,6 +8,12 @@ public class Employe {
     private double nbreHeuresSemaine;
     private int anciennete;
 
+    static double assuranceEmploi = 0.111;
+    static double fondsPension = 0.0136;
+    static double cotisationSyndicale = 20.00;
+    static double impotFederal = 0.19;
+    static double impotProvincial = 0.20;
+
     // constante(s): static final UPPER_SNAKE_CASE
     // EX: public static final int UNE_CONSTANTE = 3;
 
@@ -19,4 +25,21 @@ public class Employe {
         this.nbreHeuresSemaine = nbreHeuresSemaine;
         this.anciennete = anciennete;
     }
+
+    public double salaireBrut() {
+        return salaireHoraire * nbreHeuresSemaine;
+    }
+
+    public double salaireNetAvantImpot() {
+        double salaireBrut = salaireBrut();
+        double salaireNet = salaireBrut() - (assuranceEmploi * salaireBrut + fondsPension * salaireBrut + cotisationSyndicale);
+        return (salaireNet > 0 ? salaireNet : 0);
+    }
+
+    public double impotFederal() {
+        double salaireNet = salaireNetAvantImpot();
+        double impot = salaireNet * impotFederal;
+        return impot;
+    }
+
 }
